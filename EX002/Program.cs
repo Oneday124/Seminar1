86e5df01d808359ad83e9dabe8b17cd95f5fe4b7
Nikolay <Onedaydejavu124@mail.ru>
2022-08-24 22:30:09 +0700

    Определение меньшего из 2х чисел

---
+++ EX002/Program.cs
@@ -0,0 +1,16 @@
+﻿// Даны 2 числа показать большее и меньшее
+
+Console.Write("Введите число: ");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число: ");
+int y = Convert.ToInt32(Console.ReadLine());
+if(x < y)
+{
+    Console.WriteLine("Меньшее число: " + x);
+    Console.WriteLine("Большее число: " + y);
+}
+else
+{
+    Console.WriteLine("Меньшее число: " + y);
+    Console.WriteLine("Большее число " + x);
+}
