ca6682e750d58d0399b79e87d478125c8e0cecf9
Nikolay <Onedaydejavu124@mail.ru>
2022-08-24 23:32:23 +0700

    Показать числа от -N до N

---
+++ EX004/Program.cs
@@ -0,0 +1,29 @@
+﻿// Показать числа от -N до N
+
+Console.WriteLine("Введите число: ");
+int numberA = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число: ");
+int numberB = int.Parse(Console.ReadLine());
+
+int max = 0;
+int min = 0;
+
+if(numberA <= numberB)
+{
+    max = numberB;
+    min = numberA;
+}
+else
+{
+    max = numberA;
+    min = numberB;
+}
+
+Console.WriteLine("min = " + min);
+Console.WriteLine("max = " + max);
+
+while(min -1 < max)
+{
+    Console.WriteLine(min);
+    min++;
+}
