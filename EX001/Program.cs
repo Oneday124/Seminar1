df1c4f2293233f253ebc64053d7228e9a781e48d
Nikolay <Onedaydejavu124@mail.ru>
2022-08-24 22:01:11 +0700

    Перевод строки в чило

--- EX001/Program.cs
+++ EX001/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// По двум заданным числам проверять является ли первое квадратом второго
+
+Console.Write("Введите первое число: ");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int y = Convert.ToInt32(Console.ReadLine());
+if(x == y * y)
+{
+    Console.WriteLine("True");
+}
+else
+{
+    Console.WriteLine("false");
+}
