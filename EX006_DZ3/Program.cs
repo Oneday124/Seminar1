c290145824433eddd2202c278551a06339c1d29e
Nikolay <Onedaydejavu124@mail.ru>
2022-08-25 22:42:50 +0700

    Найти максимальное из трех чисел

--- EX006_DZ3/Program.cs
+++ EX006_DZ3/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Найти максимальное из трех чисел
+
+Console.WriteLine("Введите первое число: ");
+int first_number = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int second_number = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите третье число: ");
+int third_number = int.Parse(Console.ReadLine());
+int max = 0;
+
+if(first_number > second_number)
+{
+    max = first_number;
+}
+else
+{
+    max = second_number;
+}
+if(max > third_number)
+{
+    max = max;
+}
+else
+{
+    max = third_number;
+}
+
+Console.WriteLine(max);
