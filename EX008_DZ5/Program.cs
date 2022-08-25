34279a93b33b96854544b354864001f20159cc09
Nikolay <Onedaydejavu124@mail.ru>
2022-08-25 23:04:59 +0700

    Показать четные числа от 1 до N

---
+++ EX008_DZ5/Program.cs
@@ -0,0 +1,17 @@
+﻿// Показать четные числа от 1 до N
+
+Console.WriteLine("Enter the number: ");
+int N = int.Parse(Console.ReadLine());
+int i = 1;
+while(i < N)
+{
+    if(i % 2 == 0)
+    {
+        Console.WriteLine(i);
+        i++;
+    }
+    else
+    {
+        i++;
+    }
+}
