54165eb32fd41dc1472288ccbe4d0583a605fa3a
Nikolay <Onedaydejavu124@mail.ru>
2022-08-25 23:45:01 +0700

    Выяснить, кратно ли число заданному, если нет, вывести остаток.

---
+++ EX012_DZ9/Program.cs
@@ -0,0 +1,14 @@
+﻿// Выяснить, кратно ли число заданному, если нет, вывести остаток.
+
+Console.WriteLine("Enter the number: ");
+int divider = int.Parse(Console.ReadLine());
+int dividend = new Random().Next(0, 1000);
+Console.WriteLine("Dividend: " + dividend);
+if(dividend % divider == 0)
+{
+    Console.WriteLine("The number is a multiple");
+}
+else
+{
+    Console.WriteLine("Number is not multiple. Remainder of the division: " + dividend % divider);
+}
