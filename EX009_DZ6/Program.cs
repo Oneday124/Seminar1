2d4e3152382754fc833528732f437883251a7598
Nikolay <Onedaydejavu124@mail.ru>
2022-08-25 23:11:58 +0700

    Показать вторую цифру трёхзначного числа

---
+++ EX009_DZ6/Program.cs
@@ -0,0 +1,8 @@
+﻿// Показать вторую цифру трёхзначного числа
+
+Console.WriteLine("Введите трехзначное число: ");
+int number = int.Parse(Console.ReadLine());
+int outcome = number % 100 / 10;
+
+Console.Write(outcome);
+
