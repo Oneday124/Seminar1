e3ea939ab6e2691fc6f70b28d3d4ee3d8b35ab78
Nikolay <Onedaydejavu124@mail.ru>
2022-10-22 20:13:24 +0700

    Удалить вторую цифру случайного трёхзначного числа

--- EX011_DZ8/Program.cs
+++ EX011_DZ8/Program.cs
@@ -1,15 +1,24 @@
 ﻿// Удалить вторую цифру трёхзначного числа
+int Random ()
+{
+    int number = new Random().Next(99,1000);
+    return number;
+}
+void DelSecondNun (int number)
+{
+    if(number / 1000 > 0 || number / 100 == 0)
+    {
+        Console.WriteLine("Введенное число не является трехзначным");
+    }
+    else
+    {
+        int first = number / 100;
+        int second = number % 100 / 10;
+        int third = number % 10;
+        Console.Write($"{first}{third}");
+    }
+    }
 
-Console.WriteLine("Введите трехзначное число: ");
-int number = int.Parse(Console.ReadLine() !);
-if(number / 1000 > 0 || number / 100 == 0)
-{
-    Console.WriteLine("Введенное число не является трехзначным");
-}
-else
-{
-    int first = number / 100;
-    int second = number % 100 / 10;
-    int third = number % 10;
-    Console.Write($"{first}{third}");
-}
+int number = Random();
+System.Console.WriteLine($"Случайное число: {number}");
+DelSecondNun(number);
