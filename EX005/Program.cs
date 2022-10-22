fdf1b141d66f74df19c37990dca963af11948f75
Nikolay <Onedaydejavu124@mail.ru>
2022-10-22 17:32:57 +0700

    Показать последнюю цифру трехзначного числа

--- EX005/Program.cs
+++ EX005/Program.cs
@@ -1,5 +1,9 @@
 ﻿// Показать последнюю цифру трехзначного числа
 
 Console.WriteLine("Введите трехзначное число: ");
-int number = int.Parse(Console.ReadLine());
-Console.WriteLine(number % 10);
+int number = int.Parse(Console.ReadLine() !);
+if(number / 1000 > 0 || number / 100 == 0)
+{
+    Console.Write("Введенное число не трахзначное");
+}
+else Console.WriteLine(number % 10);
