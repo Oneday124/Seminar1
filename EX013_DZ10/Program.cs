393bd1182e5418938f10e71225e87e7da4ca5762
Nikolay <Onedaydejavu124@mail.ru>
2022-10-22 20:20:16 +0700

    Найти третью цифру числа или сообщить, что её нет

--- EX013_DZ10/Program.cs
+++ EX013_DZ10/Program.cs
@@ -1,19 +1,25 @@
 ﻿// Найти третью цифру числа или сообщить, что её нет
-
-int number = new Random().Next(1, 1000);
-Console.WriteLine($"Number: {number}");
-if(number < 100)
+int Random()
 {
-    Console.WriteLine("There is not third");
+    int number = new Random().Next(1, 10000);
+    return number;
 }
-else
+void ThirdDigitNumber(int number)
 {
-    while(number > 1000)
+    if(number < 100)
     {
-        number = number / 10;
+        Console.WriteLine("There is not third");
     }
-Console.WriteLine($"The third digit of the number = {number % 10}");
+    else
+    {
+        while(number > 1000)
+        {
+            number = number / 10;
+        }
+    Console.WriteLine($"The third digit of the number = {number % 10}");
+    }
 }
-
-
+int number = Random();
+System.Console.WriteLine($"Random number is {number}");
+ThirdDigitNumber(number);
 
