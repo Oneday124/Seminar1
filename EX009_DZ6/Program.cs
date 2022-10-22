15d0c4131bc39568520a435cccfd43cfd49e3325
Nikolay <Onedaydejavu124@mail.ru>
2022-10-22 20:04:35 +0700

    Показать вторую цифру трёхзначного числа

--- EX009_DZ6/Program.cs
+++ EX009_DZ6/Program.cs
@@ -1,14 +1,21 @@
 ﻿// Показать вторую цифру трёхзначного числа
+int ReturnThirdNumber(int number)
+{
+    if(number / 1000 > 0 || number / 100 == 0)
+    {
+        while(number / 1000 > 0)
+        {
+            number = number / 10;
+        }
+        return number % 100 / 10;
+    }
+    else
+    {
+        return number % 100 / 10;
+    }
+}
 
 Console.WriteLine("Введите трехзначное число: ");
 int number = int.Parse(Console.ReadLine() !);
-if(number / 1000 > 0 || number / 100 == 0)
-{
-    Console.WriteLine("Введенное число не является трехзначным");
-}
-else
-{
-    int outcome = number % 100 / 10;
-    Console.Write(outcome);
-}
+Console.WriteLine($"{ReturnThirdNumber(number)}");
 
