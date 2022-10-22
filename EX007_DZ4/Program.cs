f236f9492994b53ceec6a39b24d290b5a5bbd453
Nikolay <Onedaydejavu124@mail.ru>
2022-10-22 17:37:17 +0700

    Выяснить является ли число чётным

--- EX007_DZ4/Program.cs
+++ EX007_DZ4/Program.cs
@@ -1,12 +1,12 @@
 ﻿// Выяснить является ли число чётным
 
 Console.WriteLine("Enter the number: ");
-int number = int.Parse(Console.ReadLine());
+int number = int.Parse(Console.ReadLine() !);
 if(number % 2 == 0)
 {
     Console.WriteLine("Even number");
 }
 else
 {
-    Console.WriteLine("odd number");
+    Console.WriteLine("Odd number");
 }
