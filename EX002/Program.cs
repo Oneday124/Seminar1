6aa355b323d521747c79175fc633468e724aa773
Nikolay <Onedaydejavu124@mail.ru>
2022-10-22 17:18:45 +0700

    Даны 2 числа показать большее и меньшее

--- EX002/Program.cs
+++ EX002/Program.cs
@@ -1,39 +1,17 @@
 ﻿// Даны 2 числа показать большее и меньшее
 
 Console.Write("Введите число: ");
-int x = Convert.ToInt32(Console.ReadLine());
+int x = int.Parse(Console.ReadLine() !);
 Console.Write("Введите число: ");
-int y = Convert.ToInt32(Console.ReadLine());
+int y = int.Parse(Console.ReadLine()!);
 if(x < y)
 {
-    Console.WriteLine("Меньшее число: " + x);
-    Console.WriteLine("Большее число: " + y);
+    Console.WriteLine($"Меньшее число: {x}");
+    Console.WriteLine($"Большее число: {y}");
 }
 else
 {
-    Console.WriteLine("Меньшее число: " + y);
-    Console.WriteLine("Большее число " + x);
+    Console.WriteLine($"Меньшее число: {y}");
+    Console.WriteLine($"Большее число: {x}");
 }
 
-//Вариант 2
-
-int numberA = new Random().Next(1, 10);
-Console.WriteLine("Число 1: " + numberA);
-int numberB = new Random().Next(1, 10);
-Console.WriteLine("Число 2: " + numberB);
-
-int max = 0;
-int min = 0;
-if(numberA < numberB)
-{
-    max = numberB;
-    min = numberA;
-}
-else
-{
-    max = numberA;
-    min = numberB;
-}
-
-Console.WriteLine("min = " + min);
-Console.WriteLine("max = " + max);
