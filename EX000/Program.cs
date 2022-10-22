ef8767f315da6d13a3caaaec4848954259c10fd3
Nikolay <Onedaydejavu124@mail.ru>
2022-10-22 16:34:41 +0700

    Напишите программу, которая на вход принимает число и выдает его квадрат

--- EX000/Program.cs
+++ EX000/Program.cs
@@ -1,5 +1,5 @@
 ﻿// Напишите программу, которая на вход принимает число и выдает его квадрат
 
 Console.Write("Веедите число: ");
-int userNumber = int.Parse(Console.ReadLine() ??"0");
-System.Console.WriteLine($"Квадрат числа {userNumber} = {userNumber*userNumber}");
+int userNumber = int.Parse(Console.ReadLine() ?? "0");
+System.Console.WriteLine($"Квадрат числа {userNumber} = {userNumber * userNumber}");
