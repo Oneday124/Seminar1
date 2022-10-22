ce0db8d21632db9ce8d6f0293db6f10df8fabbc2
Nikolay <Onedaydejavu124@mail.ru>
2022-10-22 19:09:46 +0700

    Дано число из отрезка [10, 99]. Показать наибольшую цифру числа

--- EX010_DZ7/Program.cs
+++ EX010_DZ7/Program.cs
@@ -1,16 +1,18 @@
 ﻿// Дано число из отрезка [10, 99]. Показать наибольшую цифру числа
+int BigDigit (int number)
+{
+    int first_number = number / 10;
+    int second_number = number % 10;
+        if(first_number > second_number)
+        {
+            return first_number;
+        }
+        else
+        {
+            return second_number;
+        } 
+}
 
 int number = new Random().Next(10, 99);
 Console.WriteLine($"Число: {number}");
-
-int first_number = number / 10;
-int second_number = number % 10;
-
-if(first_number > second_number)
-{
-    Console.WriteLine(first_number);
-}
-else
-{
-    Console.WriteLine(second_number);
-}
+Console.WriteLine($"Наибольшая цифра числа = {BigDigit(number)}");
